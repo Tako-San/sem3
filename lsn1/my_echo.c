a681d8f11fb178aa82d9dcc47e966f1bdcf0b813
Tako-San <dxe13@mail.ru>
2020-09-04 23:10:34 +0400

    mistakes corrected

--- lsn1/my_echo.c
+++ lsn1/my_echo.c
@@ -14,9 +14,12 @@ void echo(int ac, char ** av)
     int no_nl = 0, i = 1;
 
     if(ac < 2)
+    {
+        printf("\n");
         return;
+    }
 
-    if(!strcmp(av[i], "-n"))
+    if(strcmp(av[i], "-n") == 0)
         no_nl = 1, ++i;
 
     for( ; i < ac; i++)
