c06934e85677b16742643dcacc8d1a182dbc8b48
Tako-San <dxe13@mail.ru>
2020-09-08 19:15:05 +0300

    Codestyle fixes

--- lsn1/my_echo.c
+++ lsn1/my_echo.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
-void echo(int ac, char ** av)
+void echo( int ac, char ** av )
 {
     int no_nl = 0, i = 1;
 
-    if(ac < 2)
+    if (ac < 2)
     {
         printf("\n");
         return;
     }
 
-    if(strcmp(av[i], "-n") == 0)
+    if (strcmp(av[i], "-n") == 0)
     {
         no_nl = 1;
        	++i;
     }
 
-    for( ; i < ac; i++)
+    for ( ; i < ac; i++)
         printf("%s%s", av[i], (i == ac - 1) ? "" : " ");
 
     printf("%s", no_nl ? "" : "\n");
 }
 
-int main(int ac, char ** av)
+int main( int ac, char ** av )
 {
     echo(ac, av);
     return 0;
