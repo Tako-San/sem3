ae18c59899e188202fb6b8b1f7f47a1a34cb9fad
Tako-San <dxe13@mail.ru>
2020-09-18 18:41:08 +0300

    TODO's done

--- lsn3/mycat.c
+++ lsn3/mycat.c
@@ -1,34 +1,49 @@
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-
 #include <unistd.h>
-
-
-#include <errno.h>
 #include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
-
 #include <stdbool.h>
 
 #define MAX_MEM_SIZE 4096
 #define MAX_ACCESS   0777
 
-bool read_n_write_to_stdout( int fd_in, char * buffer )
+ssize_t safe_write( int fd, const void * buf, size_t count )
 {
-    ssize_t r_num, w_num;
-    // TODO: добавить цикл
-    r_num = read(fd_in, buffer, MAX_MEM_SIZE);
-    if (r_num < 0)
-        return false; // TODO: добавить вывод через perror
+    ssize_t w_num = 1;
+    size_t to_wr = count;
+    const char * cur_buf = buf;
 
-    w_num = write(STDOUT_FILENO, buffer, r_num);
-    if (w_num < 0)
+    while (w_num > 0)
     {
-        perror("stdout");
-        return false;
-    } // TODO: обёртка над write
+        w_num = write(fd, cur_buf, to_wr);
+        cur_buf += w_num;
+        to_wr -= w_num;
+    }
+
+    return w_num;
+}
+
+bool read_n_write( int fd_in, int fd_out, char * buffer )
+{
+    ssize_t r_num = 1, w_num;
+
+    while (r_num > 0)
+    {
+        r_num = read(fd_in, buffer, MAX_MEM_SIZE);
+        if (r_num < 0)
+        {
+            perror("");
+            return false;
+        }
+
+        w_num = safe_write(fd_out, buffer, r_num);
+        if (w_num < 0)
+        {
+            perror("");
+            return false;
+        }
+    }
 
     return (w_num > 0 && r_num > 0);
 }
@@ -39,10 +54,9 @@ int main( int argc, char ** argv )
     char buf[MAX_MEM_SIZE];
 
 
-    //
     if (argc == 1)
     {
-        while (read_n_write_to_stdout(STDIN_FILENO, buf));
+        read_n_write(STDIN_FILENO, STDOUT_FILENO, buf);
 
         return 0;
     }
@@ -50,14 +64,17 @@ int main( int argc, char ** argv )
     for (int i = 1; i < argc; ++i)
     {
         int fd = open(argv[i], O_RDONLY, MAX_ACCESS);
+
         if (fd < 0)
         {
             perror(argv[i]);
             continue;
         }
 
-        read_n_write_to_stdout(fd, buf);
-        close(fd);  // TODO: catch errors in close
+        read_n_write(fd, STDOUT_FILENO, buf);
+
+        if (close(fd) < 0)
+            perror(argv[i]);
     }
 
     return 0;
