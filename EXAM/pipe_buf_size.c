389423198abae882bb303531aadc867f2477fab4
Tako-San <dxe13@mail.ru>
2020-12-12 14:29:03 +0300

    Exam started

--- EXAM/pipe_buf_size.c
+++ EXAM/pipe_buf_size.c
@@ -1,7 +1,21 @@
+#define _GNU_SOURCE
+
+#include <fcntl.h>
+#include <unistd.h>
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main( int ac, char ** av )
 {
+  int pipefd[2] = {};
+
+  if (pipe2(pipefd, 0) < 0)
+  {
+    perror("Pipe open error");
+    exit(1);
+  }
+
   return 0;
 }
 
