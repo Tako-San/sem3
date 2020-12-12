8d8c6b8f73e480351988dcc589cdd6539695ed7a
Tako-San <dxe13@mail.ru>
2020-12-12 15:36:39 +0300

    Exam

--- EXAM/pipe_buf_size2.c
+++ EXAM/pipe_buf_size2.c
@@ -22,7 +22,7 @@ int main( )
 
   int counter;
 
-  for (counter = 0; poll(&fds, 1, 42); ++counter)
+  for (counter = 0; poll(&fds, 1, 42) > 0; ++counter)
     write(pipefd[1], "x", 1);
 
   printf("Pipe size: %d bytes\n", counter);
