de7051ec7df13159f36db6cf2d87edff7e8a95c7
Tako-San <dxe13@mail.ru>
2020-12-12 14:46:05 +0300

    Exam

--- EXAM/pipe_buf_size.c
+++ EXAM/pipe_buf_size.c
@@ -10,7 +10,7 @@ int main( )
 {
   int pipefd[2] = {};
 
-  if (pipe2(pipefd, 0) < 0)
+  if (pipe2(pipefd, O_NONBLOCK) < 0)
   {
     perror("Pipe open error\n");
     exit(1);
@@ -19,7 +19,7 @@ int main( )
   int counter;
   char x = 'x';
 
-  for (counter = 0; write(pipefd[0], &x, 1) > 0; ++counter);
+  for (counter = 0; write(pipefd[1], &x, 1) > 0; ++counter);
 
   printf("Pipe size: %d\n", counter);
 
