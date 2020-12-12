6f868cd763d489da351489a0591e17f56d475b2c
Tako-San <dxe13@mail.ru>
2020-12-12 14:36:29 +0300

    Exam started

--- EXAM/pipe_buf_size.c
+++ EXAM/pipe_buf_size.c
@@ -6,15 +6,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main( int ac, char ** av )
+int main( )
 {
   int pipefd[2] = {};
 
   if (pipe2(pipefd, 0) < 0)
   {
-    perror("Pipe open error");
+    perror("Pipe open error\n");
     exit(1);
   }
+
+  int counter;
+
+  for (counter = 0; write(pipefd[1], "x", 1); ++counter);
+
+  printf("Pipe size: %d", counter);
 
   return 0;
 }
