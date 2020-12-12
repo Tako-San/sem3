ed409376ef3ba77a47ef62224a375f84104a2816
Tako-San <dxe13@mail.ru>
2020-12-12 15:20:47 +0300

    Exam

--- EXAM/pipe_buf_size2.c
+++ EXAM/pipe_buf_size2.c
@@ -1,11 +1,32 @@
-#include <stdio.h>
+#define _GNU_SOURCE
 
-/*
- * Смотреть переполнение через poll
- */
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <poll.h>
 
 int main( )
 {
+  int pipefd[2] = {};
+
+  if (pipe2(pipefd, 0) < 0)
+  {
+    perror("Pipe open error\n");
+    exit(1);
+  }
+
+  struct pollfd fds = {pipefd[1]};
+
+  int counter;
+
+  for (counter = 0; poll(&fds, 1, -1); ++counter)
+    write(pipefd[1], "x", 1);
+
+  printf("Pipe size: %d bytes\n", counter);
+
   return 0;
 }
 
